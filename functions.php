2c9727c63cd6461c4bc88f39b467a708ed223a6f
PeteM <github@thrap.co.uk>
2015-02-17 22:39:45 +0000

    analytics
    upgrade to universal + add content grouping function

--- functions.php
+++ functions.php
@@ -60,22 +60,35 @@ add_action( 'wp_enqueue_scripts', 'harveys_scripts_styles',100 );
 
 
 /*----ADD ANALYTICS----*/
-function ga(){ ?>
-     <script type="text/javascript">
-    
-       var _gaq = _gaq || [];
-       _gaq.push(['_setAccount', 'UA-6452121-1']);
-       _gaq.push(['_trackPageview']);
-    
-       (function() {
-         var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-         ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-         var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-       })();
-    
-     </script>
-<?php }
+function ga() 
+{
+?>
+    <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-6452121-1', 'auto');
+  <?php echo (readContentGroupMeta()); ?>
+  ga('send', 'pageview');
+
+</script>
+<?php
+}
+function readContentGroupMeta()
+{
+global $post;
+    if ($contentGroup=get_post_meta($post->ID, 'contentgroup',true)) {
+        if ($contentGroup=="product")
+        {return "ga('set', 'contentGroup1', 'Product');";}
+        else 
+	{return "ga('set', 'contentGroup1', 'Product');";}	
+	}
+        {return'';}       
+}
 add_action('wp_head', 'ga','50');
+
 
 function remove_twentytwelve_actions() {
     remove_action('body_class', 'twentytwelve_body_class');
